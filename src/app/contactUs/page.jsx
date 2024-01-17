255e665f15194b67fe7bb8afee1446b6f42bfcf0
sahan.perera <sahan.perera@rootcodelabs.com>
2024-01-18 02:21:28 +0530

    chore: added some basic files for the initialization

---
+++ src/app/contactUs/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+
+const ContactUs = () => {
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        <div className="container h-[100vh] bg-white flex items-center justify-center">
+          <p className="text-4xl font-bold">Contact Us Page</p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ContactUs;
