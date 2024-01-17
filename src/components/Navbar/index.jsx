85c8ea102ee71d94bcceef739efa37200b481572
sahan.perera <sahan.perera@rootcodelabs.com>
2024-01-18 02:32:48 +0530

    fix: vercel deployment error fixed

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -2,7 +2,7 @@
 import { useState } from "react";
 import Link from "next/link";
 
-export default function index() {
+export default function Navbar() {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   const toggleMobileMenu = () => {
