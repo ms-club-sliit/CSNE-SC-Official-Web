b2d13a6b766736ed130fe80c11dee1e9ec821a31
Sujitha Srikanthan <sujikanthan2001@gmail.com>
2024-01-21 22:09:12 +0530

    feat:created shape and customized colors

--- src/components/HeroSection/index.jsx
+++ src/components/HeroSection/index.jsx
@@ -3,27 +3,21 @@ import React from "react";
 const HeroSection = () => {
   return (
     <div className="relative flex flex-col lg:flex-row">
-      <div className="lg:text-left text-center lg:pl-16 pt-16 lg:flex-1 relative z-10">
-        <h1 className="text-4xl lg:text-5xl font-bold leading-tight">
+      <div className="lg:text-left text-center mt-4 lg:pl-16 lg:flex-1 relative z-10">
+        <h1 className="text-4xl lg:text-4xl font-bold leading-tight">
           CSNE <br /> Student <br /> Community
         </h1>
-        <p className="text-lg mt-6 lg:mt-10 text-gray-700 max-w-sm mx-auto lg:mx-0">
+        <p className="mt-6 lg:mt-10 text-gray-700 max-w-sm mx-auto lg:mx-0">
           Welcome to the hub of collaboration, learning, and networking for
           computer science and network engineering students. Together, we shape
           the future of technology.
         </p>
-        <button
-          className="text-white px-6 py-3 rounded-full mt-10 text-xl"
-          style={{ backgroundColor: "#1D1163" }}
-        >
+        <button className="bg-custom text-white px-6 py-3 rounded-full mt-8 text-lg">
           Join Now
         </button>
       </div>
 
-      <div
-        className="lg:flex-1 mt-10 lg:ml-0 mx-auto relative z-10"
-        style={{ backgroundColor: "transparent" }}
-      >
+      <div className="lg:flex-1 lg:ml-0 mx-auto relative z-10">
         <div
           className="bg-blue-200 rounded-full h-10 w-16 z-20"
           style={{
@@ -44,6 +38,14 @@ const HeroSection = () => {
             }}
           />
         </div>
+        <div
+          className="bg-beige absolute right-0 mb-4 mr-8"
+          style={{
+            width: "80px",
+            height: "50px",
+            borderRadius: "0% 0% 50% 0%",
+          }}
+        ></div>
       </div>
     </div>
   );
