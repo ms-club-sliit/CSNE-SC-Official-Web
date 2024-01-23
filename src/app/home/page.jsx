17a5c6a5247ed7c3f59ebb751284666fad786c08
Ishara Madusanka <isharamadusanka410@gmail.com>
2024-01-23 22:16:30 +0530

    blog and video section created without icons

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,13 +1,17 @@
 "use client";
+import BlogVideoSection from "@/components/home/blogVideoSection/BlogVideoSection";
 import React from "react";
 
 const Home = () => {
   return (
     <>
-      <div className="flex items-center justify-center">
+      {/* <div className="flex items-center justify-center">
         <div className="container h-[100vh] bg-white flex items-center justify-center">
           <p className="text-4xl font-bold">Home Page</p>
         </div>
+      </div> */}
+      <div className="">
+        <BlogVideoSection />
       </div>
     </>
   );
---
+++ src/components/home/blogVideoSection/BlogVideoSection.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import VideoSection from "./VideoSection";
+import AllBlogSection from "./AllBlogSection";
+import BlogVideoCard from "./BlogVideoCard";
+
+const BlogVideoSection = () => {
+  return (
+    <div>
+      <AllBlogSection />
+      <VideoSection />
+    </div>
+  );
+};
+
+export default BlogVideoSection;
