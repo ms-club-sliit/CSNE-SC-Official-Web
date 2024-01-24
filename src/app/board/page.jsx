e958d7923b262e8617e0ecf8d6c1c1972aa9ead1
TD JAYADEERA <edu.tharusha@gmail.com>
2024-01-24 10:23:34 +0530

    feature/Board init

--- src/app/board/page.jsx
+++ src/app/board/page.jsx
@@ -1,13 +1,90 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
+import Teamcard from "@/components/TeamCard";
+import BoardData from '@/data/BoardData';
 
 const Board = () => {
+
+
+  const [ismore, setIsMore] = useState(true);
+  const [dropdown,setDropwown]=useState(false);
+  const [year,setYear] = useState('Y2023');
+
+  // Limit the number of items to display
+  const Top3Position =  BoardData[year].TopPosition.slice(0, 3);
+  const TopPosition = BoardData[year].TopPosition.slice(3);
+  const member = BoardData[year].member;
+
+
+
   return (
     <>
-      <div className="flex items-center justify-center">
-        <div className="container h-[100vh] bg-white flex items-center justify-center">
-          <p className="text-4xl font-bold">Board Page</p>
+      <div className="mx-10 flex flex-col items-center pt-10">
+        <h1 className="font-bold text-3xl text-center text-[6vw]">OUR TEAM</h1>
+        <hr className=" mt-[5vw] w-full" />
+
+        
+        <div className="flex md:justify-end justify-center  w-full p-5 relative z-50 ">
+      
+
+            <div className="dropdown inline-block relative">
+              <button onClick={()=> setDropwown(!dropdown)} className="bg-[#1d1163] text-white font-semibold py-2 px-4 rounded inline-flex items-center">
+                <span className="mr-1">{year}</span>
+                <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" /> </svg>
+              </button>
+              {(dropdown) ? (
+                <ul className="dropdown-menu absolute w-full    text-white pt-1">
+                <li className=""><button onClick={()=> {setYear('Y2023');setDropwown(!dropdown)}} className="rounded-t bg-[#1d1163]  w-full  py-2 px-4 block whitespace-no-wrap" >2023</button></li>
+                <li className=""><button onClick={()=> {setYear('Y2022');setDropwown(!dropdown)}} className=" bg-[#1d1163] w-full     py-2 px-4 block whitespace-no-wrap" >2022</button></li>
+                <li className=""><button onClick={()=> {setYear('Y2021');setDropwown(!dropdown)}} className="bg-[#1d1163] w-full  py-2 px-4   block whitespace-no-wrap" >2021</button></li>
+                <li className=""><button onClick={()=> {setYear('Y2020');setDropwown(!dropdown)}} className="rounded-b w-full  bg-[#1d1163]    py-2 px-4 block whitespace-no-wrap" >2020</button></li>
+              </ul>
+              ): ''}
+            </div>
+
+          
         </div>
+
+        <div className="md:flex    flex-wrap  justify-center items-baseline w-full hidden  md:w-[70vw] top_board">
+          {Top3Position.map((user) => {
+            if (user.position == "PRESIDENT") {
+              return <Teamcard key={Math.random()} height={440} width={440} name={user.name} image={user.image} position={user.position} link={user.link} />
+            } else {
+              return <Teamcard key={Math.random()} height={170} width={170} name={user.name} image={user.image} position={user.position} link={user.link} />
+            }
+          })}
+        </div>
+
+        <div className="flex    flex-wrap  justify-center items-baseline w-full md:hidden  md:w-[70vw] top_board">
+          {Top3Position.map((user) => {
+             return <Teamcard key={Math.random()} height={170} width={170} name={user.name} image={user.image} position={user.position} link={user.link} />
+            
+          })}
+        </div>
+
+        <div className="grid    md:grid-cols-5 gap-x-10   mx-10  place-items-baseline place-content-center">
+          {TopPosition.map((user) => (
+            <Teamcard key={Math.random()} height={130} width={130} name={user.name} image={user.image} position={user.position} link={user.link} />
+          ))}
+        </div>
+
+
+
+        {(ismore) ? (
+          <div className="flex px-[45px] w-full justify-end">
+            <button onClick={() => { setIsMore(false) }} className="w-[30px] text-[18px]     font-bold rounded-full h-[30px] bg-blue-900 flex justify-center items-center text-white">{">"}</button>
+          </div>
+        ) :
+          <div className="grid md:grid-cols-5 gap-x-10   mx-10  place-items-baseline place-content-center">
+            {member.map((user) => (
+              <Teamcard key={Math.random()} height={130} width={130} name={user.name} image={user.image} position={user.position} link={user.link} />
+            ))}
+          </div>
+        }
+
+        <hr className=" mt-5 w-full" />
+
+        <br></br>
       </div>
     </>
   );
---
+++ src/components/TeamCard/index.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+function index(props) {
+    return (
+        <div className='md:w-[18vw]  p-5 text-center w-full   flex flex-col items-center relative '>
+            <Image className='rounded-full mb-3' src={props.image} width={props.width} height={props.height} alt='image' />
+            <h1 className='font-bold text-[4vw] md:text-[1.5vw] ' >{props.position}</h1>
+            <p className='mb-[10px] font-semibold md:text-[1.3vw] text-[3vw] '>{props.name}</p>
+            <div className='flex items-center ' >
+                <a href={props.link.facebook}>
+                    <svg className='md:w-[2vw] w-[5vw]' xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48">
+                        <path fill="#039be5" d="M24 5A19 19 0 1 0 24 43A19 19 0 1 0 24 5Z"></path><path fill="#fff" d="M26.572,29.036h4.917l0.772-4.995h-5.69v-2.73c0-2.075,0.678-3.915,2.619-3.915h3.119v-4.359c-0.548-0.074-1.707-0.236-3.897-0.236c-4.573,0-7.254,2.415-7.254,7.917v3.323h-4.701v4.995h4.701v13.729C22.089,42.905,23.032,43,24,43c0.875,0,1.729-0.08,2.572-0.194V29.036z"></path>
+                    </svg>
+                </a>
+                <a href={props.link.likened}>
+                    <svg className='md:w-[2vw] w-[5vw]' xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 48 48">
+                        <path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"></path>
+                    </svg>
+                </a>
+                <a href={props.link.instragram}>
+                    <svg className='md:w-[2vw] w-[5vw]' xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 32 32">
+                        <path d="M 11.46875 5 C 7.917969 5 5 7.914063 5 11.46875 L 5 20.53125 C 5 24.082031 7.914063 27 11.46875 27 L 20.53125 27 C 24.082031 27 27 24.085938 27 20.53125 L 27 11.46875 C 27 7.917969 24.085938 5 20.53125 5 Z M 11.46875 7 L 20.53125 7 C 23.003906 7 25 8.996094 25 11.46875 L 25 20.53125 C 25 23.003906 23.003906 25 20.53125 25 L 11.46875 25 C 8.996094 25 7 23.003906 7 20.53125 L 7 11.46875 C 7 8.996094 8.996094 7 11.46875 7 Z M 21.90625 9.1875 C 21.402344 9.1875 21 9.589844 21 10.09375 C 21 10.597656 21.402344 11 21.90625 11 C 22.410156 11 22.8125 10.597656 22.8125 10.09375 C 22.8125 9.589844 22.410156 9.1875 21.90625 9.1875 Z M 16 10 C 12.699219 10 10 12.699219 10 16 C 10 19.300781 12.699219 22 16 22 C 19.300781 22 22 19.300781 22 16 C 22 12.699219 19.300781 10 16 10 Z M 16 12 C 18.222656 12 20 13.777344 20 16 C 20 18.222656 18.222656 20 16 20 C 13.777344 20 12 18.222656 12 16 C 12 13.777344 13.777344 12 16 12 Z"></path>
+                    </svg>
+                </a>
+            </div>
+        </div>
+    )
+}
+
+export default index
