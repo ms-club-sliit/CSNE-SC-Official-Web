9b0ee6b5f1a89b959b3efde88a637813fa01a0e9
UdeeshaRukshan <IT21807480@my.sliit.lk>
2024-01-27 02:35:12 +0530

    fix: fix ui issues

--- src/app/stories/page.jsx
+++ src/app/stories/page.jsx
@@ -23,14 +23,14 @@ const Stories = () => {
       <div className=" container h-full w-full bg-white  items-center justify-center p-2 sm:p-0 lg:w-full md:justify-center md:items-center lg:justify-center lg:items-center xl:w-full 2xl:w-full">
         <div className="flex flex-col items-center justify-center  w-full sm:w-full  ">
           <div className="flex w-full flex-col items-center space-y-4 sm: md:w-full lg:w-full ">
-            <div className="h-[90vh] flex flex-col-reverse  items-center justify-center w-full  sm:flex-col-reverse sm:justify-center  sm:items-center lg:flex-row lg:justify-around lg:w-[100vh] lg:h-[85vh]  xl:h-[80vh] xl:w-[120vh] 2xl:w-[85vh] 2xl:h-[40vh] 2xl:mt-[8vh] ">
+            <div className="h-[80vh] flex flex-col-reverse  items-center justify-center w-full  sm:flex-col-reverse sm:justify-center  sm:items-center sm:mb-12 sm:mt-10 lg:flex-row lg:justify-around lg:w-[140vh] lg:h-[80vh]  xl:h-[75vh] xl:w-[135vh] 2xl:w-[85vh] 2xl:h-[40vh] 2xl:mt-[8vh] ">
               <div className="w-full sm:w-3/3 lg:w-full p-4  sm:text-center sm:flex lg:flex  xl:w-3/4 lg:mb-[5vh] xl:mb-[5vh]">
                 <div className="text-wrap ... sm:flex sm:items-center sm:justify-center lg:flex ">
                   <div className="lg:w-3/4 ">
-                    <h1 className="text-4xl sm:text-4xl m-4 sm:m-8 lg:text-6xl text-black font-bold text-center sm:mr-4 xl:6xl ">
+                    <h1 className="text-4xl sm:text-4xl m-4 sm:m-8 lg:text-4xl text-black font-bold text-center sm:mr-4 xl:6xl ">
                       Our way of success
                     </h1>
-                    <p className="text-base sm:text-xl text-black leading-relaxed text-justify sm:ml-4 md:text-center lg:text-center lg:text-xl xl:5xl">
+                    <p className="text-base sm:text-xl text-black leading-relaxed text-justify sm:ml-4 md:text-center lg:text-center lg:text-lg xl:5xl 2xl:mb-[12vh]">
                       Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                       Ipsum rerum sapiente quaerat esse repellat quos
                       exercitationem laboriosam nostrum eum. Suscipit maiores
@@ -46,7 +46,7 @@ const Stories = () => {
                 width={600}
                 height={600}
                 alt="success stories"
-                className="w-3/4 border-2 rounded-2xl border-gray-300 shadow-lg  sm:mx-8 sm:mt-12 sm:ml-15  sm:w-3/5 lg:w-4/5 lg:mb-[20vh]  xl:w-4/5 "
+                className="w-3/4 border-2 rounded-2xl border-gray-300 shadow-lg  sm:mx-8 sm:mt-12 sm:ml-15  sm:w-3/5 lg:w-4/5 lg:mb-[20vh]  xl:w-4/5  2xl:h-[25vh]"
               />
             </div>
 
@@ -54,7 +54,7 @@ const Stories = () => {
               className="flex flex-col md:w-full justify-center items-center responsive-background"
               style={responsiveBackgroundImage}
             >
-              <div className="flex flex-col md:w-full  lg:w-[100vh]  xl:w-[120vh] 2xl:w-[85vh] ">
+              <div className="flex flex-col md:w-full  lg:w-[140vh]  xl:w-[126vh] 2xl:w-[85vh] ">
                 <div className="flex flex-col justify-center items-center sm:flex  space-y-4 sm:space-x-3 sm:mt-8 lg:flex-row ">
                   <Image
                     src={sampleImage2}
@@ -64,7 +64,7 @@ const Stories = () => {
                     className="w-4/5 border-2 rounded-2xl   border-gray-300 shadow-lg sm:mx-8  sm:w-3/5 lg:w-3/5 "
                   />
                   <div className="text-base sm:text-xl">
-                    <p className="text-base text-justify leading-relaxed text-black  p-4 sm:text-xl sm:ml-[9vh] sm:w-5/6">
+                    <p className="text-base text-justify leading-relaxed text-black  lg:text-lg  p-4 sm:text-xl sm:ml-[9vh] sm:w-5/6">
                       Lorem ipsum dolor sit amet consectetur adipisicing elit.
                       Facilis, facere. Tempora aliquam illum incidunt sequi
                       facilis optio architecto minus eaque accusamus dolorum.
