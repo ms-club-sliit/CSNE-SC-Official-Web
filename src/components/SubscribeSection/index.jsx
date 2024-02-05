703e748286bc6519235ac814306cc78c2e0a58f2
sahan.perera <sahan.perera@rootcodelabs.com>
2024-02-05 19:33:35 +0530

    fix: fixed minor ui related bug

--- src/components/SubscribeSection/index.jsx
+++ src/components/SubscribeSection/index.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const SubscribeSection = () => {
   return (
-    <div className="relative flex justify-center w-full mb-10 lg:my-10 bg-[]">
+    <div className="relative flex justify-center w-full mb-10 lg:my-10">
       <Image
         className="absolute h-[154px] object-cover right-[100px] top-[20%]"
         src="/static/dot_group.png"
@@ -12,18 +12,18 @@ const SubscribeSection = () => {
         height="154"
       />
       <div className="container lg:bg-[#F4F9FF] relative mx-auto w-full mx-5 my-5 rounded-[80px]">
-        <div className="lg:grid grid-cols-2 text-center lg:text-left bg-test h-max w-full rounded-[75px]">
-          <div className="hidden lg:block absolute -z-10 flex bg-orange-50 bottom-[13%] h-[65%] bottom-[-20px] left-[-60px] rotate-[90deg] rounded-br-[100px] w-[13%] transition-all duration-300" />
+        <div className="lg:grid grid-cols-2 text-center lg:text-left bg-test w-full rounded-[75px]">
+          <div className="hidden lg:block absolute -z-10 flex bg-orange-50 h-[65%] bottom-[-20px] left-[-60px] rotate-[90deg] rounded-br-[100px] w-[13%] transition-all duration-300" />
           <div className="container lg:px-5 lg:ml-10 lg:pl-10 lg:my-auto">
             <div className="text-4xl lg:text-5xl text-[40px] text-black-900 font-bold">
               Subscribe
             </div>
-            <div className="leading-[160%] lg:text-gray-600 lg:text-lg lg:text-xl w-full lg:max-w-lg mt-10 lg:mt-5">
+            <div className="leading-[160%] lg:text-gray-600 lg:text-xl w-full lg:max-w-lg mt-10 lg:mt-5">
               Get the latest updates and information regarding CSNE Club of
               SLIIT right to your inbox!
             </div>
           </div>
-          <div className="container px-5 lg:w-full lg:h-[253px] flex items-center justify-between relative w-full lg:mt-5 md:mt-0 bg-[]">
+          <div className="container px-5 lg:w-full lg:h-[253px] flex items-center justify-between relative w-full lg:mt-5 md:mt-0">
             <Image
               className="hidden lg:block absolute w-8/12 h-[253px] my-auto right-[0]"
               src="/static/img_rectangle.svg"
@@ -32,7 +32,7 @@ const SubscribeSection = () => {
               height="253"
             />
             <form className="w-full">
-              <div className="relative w-full bg-[] mt-10 bg-white shadow-xl border border-blue_gray-50 border-solid lg:flex lg:flex-row h-max inset-y-[0] items-center justify-between left-[0] lg:my-auto p-2 rounded-[34px] shadow-[10px 20px  50px 0px #00000026] w-full md:w-[84%]">
+              <div className="relative w-full mt-10 bg-white shadow-xl border border-blue_gray-50 border-solid lg:flex lg:flex-row h-max inset-y-[0] items-center justify-between left-[0] lg:my-auto p-2 rounded-[34px] shadow-[10px 20px  50px 0px #00000026] w-full md:w-[84%]">
                 <input
                   className="lg:ml-[22px] px-5 w-full lg:w-[8/12] py-2 text-base text-gray-500 border-none focus:outline-none"
                   placeholder="Enter your email address.."
