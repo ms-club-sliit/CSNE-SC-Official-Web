f300f8d874497354e8d49468862296ed2b45e7a4
sahan.perera <sahan.perera@rootcodelabs.com>
2024-02-22 20:01:43 +0530

    chore: tailwing config changes added

--- tailwind.config.js
+++ tailwind.config.js
@@ -21,42 +21,27 @@ module.exports = {
         "hero-section-button": "#1D1163",
         "hero-section-shape": "#DAE9FF",
       },
-      margin: {
-        "hero-image-margin-top": "30px",
-        "hero-section-margin-left": "89.87px",
-        "hero-section-margin-top": "70px",
-      },
-      height: {
-        "hero-section-text-height": "441.41px",
-        "hero-section-image-height": "378px",
-      },
       colors: {
         gray: { 50: "#f4f9ff", 500: "#959595", 600: "#757575" },
         orange: { 50: "#fff4da" },
         blue_gray: { 50: "#f1f1f1" },
         indigo_blue: "#1d1163",
-        whitegray: "#FBFBFB",
-        white: "#FFFFFF",
-        lightgray: "#9E9E9E",
-        darkblue: "#1D1163",
       },
       fontFamily: {
         arial: "Arial",
         avertademopecutteddemo: "Averta Demo PE Cutted Demo",
       },
-      colors: {
-        whitegray: "#FBFBFB",
-        white: "#FFFFFF",
-        gray: "#565656",
-        lightgray: "#9E9E9E",
-        darkblue: "#1D1163",
-      },
       height: {
         "57vh": "57vh",
         "80vh": "80vh",
+        "hero-section-text-height": "441.41px",
+        "hero-section-image-height": "378px",
       },
       margin: {
         "20px": "70px",
+        "hero-image-margin-top": "30px",
+        "hero-section-margin-left": "89.87px",
+        "hero-section-margin-top": "70px",
       },
       width: {
         "62%": "62%",
