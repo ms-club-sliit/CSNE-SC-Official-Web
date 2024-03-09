5f25cd72aa5c0269c3875e23bf56b660647b2438
ChillBroh <isharamadusanka410@gmail.com>
2024-03-10 01:09:25 +0530

    title alignment fixed

--- src/components/blogVideoSection/VideoSection.jsx
+++ src/components/blogVideoSection/VideoSection.jsx
@@ -8,7 +8,7 @@ const VideoSection = () => {
   return (
     <div className=" mt-4 px-4 sm:px-6  lg:px-8 ">
       <div className="flex flex-col sm:flex-row gap-8 items-center">
-        <div className="text-[38px] font-bold text-center">Our Videos</div>
+        <div className="text-[38px] ml-8 font-bold text-center">Our Videos</div>
         <div className="flex flex-col bg-[#F4F9FF] max-h-[400px] p-4 overflow-auto  sm:flex-row gap-4 sm: sm:gap-6 ">
           {data.length > 0 ? (
             data.map((item) => (
