e691578adcb071b75fb3100e51b8d9782337c2c3
Isuru Akalanka <hostakalanka@gmail.com>
2024-03-09 17:47:14 +0530

    fix invalid year

--- src/components/Events/Events.js
+++ src/components/Events/Events.js
@@ -14,7 +14,7 @@ const eventsByYear = {
 const Events = ({ year }) => {
 
     const events = eventsByYear[year];
-    
+
 
     const currentYear = new Date().getFullYear();
 
@@ -22,10 +22,10 @@ const Events = ({ year }) => {
     return (
         <section className="container ml-auto mr-auto p-4 sm:p-4 md:p-4 lg:p-0 xl:p-0" >
 
-            <h2 className="text-3xl font-semibold text-center">Check out the amazing experiences we shared <span className="text-[#1D1163]">{year}</span> at <span> <Image className="inline align-middle" width={100} height={100} src={'/static/csnesc-logo.png'}/></span></h2>
-          
+            <h2 className="text-3xl font-semibold text-center">Check out the amazing experiences we shared <span className="text-[#1D1163]">{year}</span> at <span> <Image className="inline align-middle" width={100} height={100} src={'/static/csnesc-logo.png'} /></span></h2>
+
             {
-                events.length !== 0 ? events?.map((event, key) => <Event element={key} key={key} event={event} />) :
+                events && events.length !== 0 ? events?.map((event, key) => <Event element={key} key={key} event={event} />) :
                     (year == currentYear) ? <p className="text-2xl text-center pb-5 pt-10"> Stay Tuned More Events Will Be Available In The Futrue</p>
                         : <p className="text-2xl text-center pb-5 pt-10">{year} No Events Are Available</p>
             }
