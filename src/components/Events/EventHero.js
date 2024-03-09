552d8b54bf367b5b97b47d8f26dbd3da5b272c8b
Isuru Akalanka <hostakalanka@gmail.com>
2024-03-09 21:50:00 +0530

    layout changes 2

--- src/components/Events/EventHero.js
+++ src/components/Events/EventHero.js
@@ -30,13 +30,13 @@ const EventHero = () => {
                             <p className="text-left text-4xl text-slate-800 antialiased">our students</p>
                         </div>
 
-                        <div className="absolute md:top-[250px] lg:top-20 right-40 xl:right-[400px]">
+                        <div className="absolute md:bottom-14 lg:bottom-0  lg:top-22 xl:right-[400px] right-40 ">
                             <div className="relative top-8 left-40 xl:left-[400px]">
-                                <Image className="rounded-lg shadow-lg md:w-[280px] xl:w-[520px]" alt={""} src={`/images/upcoming.jpg`} layout="intrinsic" width={400} height={220} />
+                                <Image className="rounded-lg shadow-lg md:w-[280px] lg:w-[380px] xl:w-[520px]" alt={""} src={`/images/upcoming.jpg`} layout="intrinsic" width={400} height={220} />
                             </div>
 
                             <div className="relative xl:right-41">
-                                <Image className="rounded-lg shadow-lg md:w-[400px] xl:w-[800px]" alt={""} src={`/images/group.jpg`} layout="intrinsic" width={500} height={300} />
+                                <Image className="rounded-lg shadow-lg md:w-[400px] lg:w-[500px] xl:w-[800px]" alt={""} src={`/images/group.jpg`} layout="intrinsic" width={500} height={300} />
                             </div>
                         </div>
                     </div>
