bd91bd5b6b4e916505761f80d786a9b5afabf78a
sathninduk <45420836+sathninduk@users.noreply.github.com>
2024-03-10 10:16:15 +0530

    debug: import past event data json

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -4,15 +4,10 @@ import WhoAreWe from "@/components/WhoAreWeSection";
 import SubscribeSection from "@/components/SubscribeSection";
 import PastEventsSection from "@/components/PastEventsSection";
 
+// import data
+import pastEvents from "@/data/pastEvents";
+
 const Home = () => {
-
-    // past events
-    const pastEvents = [
-        {id: 1, image: "/images/past-events/agm-23.jpg", title: "AGM-2023", description: "CSNE student committee", url: "https://google.com"},
-        {id: 2, image: "/images/past-events/netcom.png", title: "Netcom", description: "Grand Finale", url: ""},
-        {id: 3, image: "/images/past-events/dansela.jpg", title: "CSNE Dansala", description: "CSNE student community", url: ""},
-    ];
-
     return (
         <div className="flex items-center justify-center">
             <div className="text-black flex flex-col w-full">
