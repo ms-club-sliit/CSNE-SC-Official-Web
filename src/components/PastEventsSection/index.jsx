ae7f83421455a31f0431ca5018e9bc480cf12674
sahan.perera <sahan.perera@rootcodelabs.com>
2024-03-11 20:56:45 +0530

    added minor UI changes

--- src/components/PastEventsSection/index.jsx
+++ src/components/PastEventsSection/index.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import Image from "next/image";
 import ImageCarousel from "@/components/ImageCarousel";
 
-const PastEventsSection = ({pastEvents}) => {
-    return (
-        <div className="py-10">
-            <div className="text-4xl lg:text-5xl font-bold text-center lg:text-left lg:ml-24 mb-12">
-                Past Events
-            </div>
-            <div className="lg:ml-24 min-h-[300px] pr-4 pl-4">
-                <ImageCarousel slides={pastEvents}/>
-            </div>
-        </div>
-    );
+const PastEventsSection = ({ pastEvents }) => {
+  return (
+    <div className="container mx-auto my-10 px-10 lg:px-0">
+      <div className="text-4xl lg:text-5xl font-bold text-center lg:text-left mb-12">
+        Past Events
+      </div>
+      <div className="lg:ml-24 min-h-[300px]">
+        <ImageCarousel slides={pastEvents} />
+      </div>
+    </div>
+  );
 };
 
 export default PastEventsSection;
