e984723e60df5fbaf62ca5e27ca03b78303a69ed
sahan.perera <sahan.perera@rootcodelabs.com>
2024-03-12 08:33:36 +0530

    added UI changes

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn.pixabay.com",
+        port: "",
+        pathname: "/photo/2018/11/13/21/43/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
