3a12b36224b6cabb27a94b97ceeb494794a23b28
sahan.perera <sahan.perera@rootcodelabs.com>
2024-03-12 09:14:50 +0530

    added code changes for events screen

--- src/app/events/page.jsx
+++ src/app/events/page.jsx
@@ -2,12 +2,13 @@ import EventHero from "@/components/Events/EventHero";
 import Events from "@/components/Events/Events";
 
 const Page = ({ params }) => {
+
   return (
     <>
       <div className=" bg-[#FEFEFE] text-zinc-900 ">
         <div>
           <EventHero />
-          <Events year={params.year ? params.year : 2023} />
+          <Events year={params.year} />
         </div>
       </div>
     </>
--- src/components/Events/Events.jsx
+++ src/components/Events/Events.jsx
@@ -1,38 +1,54 @@
-
-import { Events21, Events23, Events22, Events24 } from "@/data/events/index.js"
+import { Events21, Events23, Events22, Events24 } from "@/data/events/index.js";
 import Event from "./Event";
 import Image from "next/image";
 
-
 const eventsByYear = {
-    2021: Events21,
-    2022: Events22,
-    2023: Events23,
-    2024: Events24,
+  2021: Events21,
+  2022: Events22,
+  2023: Events23,
+  2024: Events24,
 };
 
 const Events = ({ year }) => {
+  const events = eventsByYear[year ? year : 2023];
 
-    const events = eventsByYear[year];
+  const currentYear = new Date().getFullYear();
 
-    const currentYear = new Date().getFullYear();
+  return (
+    <section className="container ml-auto mr-auto p-4 sm:p-4 md:p-4 lg:p-0 xl:p-0">
+      <hr class="container h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
 
+      <h2 className="text-3xl font-semibold text-center">
+        Check out the amazing experiences we shared{" "}
+        <span className="text-[#1D1163]">{year}</span> at{" "}
+        <span>
+          {" "}
+          <Image
+            className="inline align-middle"
+            alt="csne logo"
+            width={100}
+            height={100}
+            src={"/static/csnesc-logo.png"}
+          />
+        </span>
+      </h2>
 
-    return (
-        <section className="container ml-auto mr-auto p-4 sm:p-4 md:p-4 lg:p-0 xl:p-0" >
-          <hr class="container h-px my-8 bg-gray-200 border-0 dark:bg-gray-700"></hr>
-
-            <h2 className="text-3xl font-semibold text-center">Check out the amazing experiences we shared <span className="text-[#1D1163]">{year}</span> at <span> <Image className="inline align-middle" alt="csne logo" width={100} height={100} src={'/static/csnesc-logo.png'} /></span></h2>
-
-            {
-                events && events.length !== 0 ? events?.map((event, key) => <Event element={key} key={key} event={event} />) :
-                    (year == currentYear) ? <p className="text-2xl text-center pb-12 pt-10 text-[#1D1163]"> Stay Tuned More Events Will Be Available In The Futrue</p>
-                        : <p className="text-2xl text-center pb-12 pt-10 text-[#1D1163]">{year} No Events Are Available</p>
-            }
-
-        </section>
-
-    )
-}
+      {events && events.length !== 0 ? (
+        events?.map((event, key) => (
+          <Event element={key} key={key} event={event} />
+        ))
+      ) : year == currentYear ? (
+        <p className="text-2xl text-center pb-12 pt-10 text-[#1D1163]">
+          {" "}
+          Stay Tuned More Events Will Be Available In The Futrue
+        </p>
+      ) : (
+        <p className="text-2xl text-center pb-12 pt-10 text-[#1D1163]">
+          {year} No Events Are Available
+        </p>
+      )}
+    </section>
+  );
+};
 
 export default Events;
