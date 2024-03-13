0001d7a7884e99c35ef2f090f82f34819fa4dbfd
ChathukaViduranga <chathuka.viduranga.bc@gmail.com>
2024-03-14 01:20:54 +0530

    completed stories

--- src/app/stories/page.jsx
+++ src/app/stories/page.jsx
@@ -3,7 +3,6 @@ import Image from "next/image";
 
 const Stories = () => {
   const sampleImage = "/images/back.png.jpeg";
-  const sampleImage2 = "/images/success-stories-1.png";
 
   return (
     <div className=" flex w-full items-center justify-center">
@@ -48,45 +47,68 @@ const Stories = () => {
               <div className="flex flex-col md:w-full  lg:w-[125vh]  xl:w-[145vh] 2xl:w-3/4 ">
                 <div className="flex flex-col justify-center items-center sm:flex  space-y-4 sm:space-x-3 sm:mt-8 lg:flex-row ">
                   <Image
-                    src={sampleImage2}
-                    width={500}
-                    height={300}
+                    src={"/images/stories/3.png"}
+                    width={1000}
+                    height={1000}
                     alt="Image 2"
                     className="w-4/5 border-2 rounded-2xl   border-gray-300 shadow-lg sm:mx-8  sm:w-3/5 lg:w-2/5 "
                   />
 
                   <div className="text-base sm:text-xl">
                     <p className="text-base text-justify leading-relaxed text-black  lg:text-lg  p-4 sm:text-xl sm:ml-[9vh] sm:w-5/6">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed nec augue non libero varius sagittis. Nulla tincidunt
-                      vestibulum libero, in eleifend risus laoreet ut. Nulla
-                      facilisi. Donec ac nisl ut arcu dignissim dictum. Praesent
-                      lacinia diam ac augue fringilla, vel mattis quam vehicula.
-                      In non est tellus. Integer sit amet bibendum sem. Nulla
-                      euismod justo a ligula hendrerit, in interdum justo
-                      laoreet.
+                      "Choosing the Computer Systems and Network Engineering
+                      (CSNE) specialization was not just a decision; it was a
+                      declaration of my passion. Despite the gender disparity in
+                      the field, I embraced my uniqueness and thrived. Upon
+                      completing my internship at Pearson, I joined IFS, where I
+                      continued to learn, grow, and contribute to the
+                      organization's success. My journey is a testament to the
+                      power of passion, perseverance, and the courage to pursue
+                      one's dreams. If there's one thing I've learned, it's that
+                      success is not just about reaching the destination; it's
+                      about embracing the journey and all the lessons it
+                      brings."
+                      <br />
                     </p>
+                    <div className="text-right mx-20 text-black  italic">
+                      - Thamali Munasinghe -
+                    </div>
                   </div>
                 </div>
                 {/* item 2 */}
                 <div className="flex flex-col-reverse justify-center  items-center sm:space-y-4 mt-10 sm:flex-col-reverse lg:flex-row lg:mt-16">
                   <div className=" text-base sm:text-xl">
-                    <p className="text-base text-justify leading-relaxed text-black  p-4 sm:text-xl sm:ml-[9vh] sm:w-5/6">
-                      Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                      Sed nec augue non libero varius sagittis. Nulla tincidunt
-                      vestibulum libero, in eleifend risus laoreet ut. Nulla
-                      facilisi. Donec ac nisl ut arcu dignissim dictum. Praesent
-                      lacinia diam ac augue fringilla, vel mattis quam vehicula.
-                      In non est tellus. Integer sit amet bibendum sem. Nulla
-                      euismod justo a ligula hendrerit, in interdum justo
-                      laoreet.
+                    <p className="text-base text-justify leading-relaxed text-black  p-4 sm:text-lg sm:ml-[9vh] sm:w-5/6">
+                      "Embarking on the path of Computer Systems and Network
+                      Engineering (CSNE) was more than just a choice for me; it
+                      was a passion ignited by curiosity and a drive for
+                      innovation. Despite the demanding nature of the field and
+                      the hurdles along the way, I remained steadfast in my
+                      commitment to excel. Through rigorous study sessions,
+                      hands-on projects, and countless late nights of coding, I
+                      transformed challenges into stepping stones for growth.
+                      Graduating with top honors from SLIIT University was just
+                      the beginning of my journey. Armed with a solid foundation
+                      and a hunger for success, I landed a dream job at a
+                      renowned tech company, where I swiftly rose through the
+                      ranks due to my exceptional performance and dedication.
+                      Today, as a leader in the field, I continue to push
+                      boundaries, mentor aspiring engineers, and contribute to
+                      groundbreaking projects that shape the future of
+                      technology. My journey underscores the significance of
+                      resilience, determination, and the relentless pursuit of
+                      excellence in achieving remarkable success in Computer
+                      Systems and Network Engineering."
                     </p>
+                    <div className="text-right mx-20 text-black italic">
+                      - Lakisuru Semasinghe -
+                    </div>
                   </div>
 
                   <Image
-                    src={sampleImage2}
-                    width={500}
-                    height={500}
+                    src={"/images/stories/2.png"}
+                    width={1000}
+                    height={1000}
                     alt="Image 3"
                     className="w-4/5 border-2 rounded-2xl border-gray-300 shadow-lg  sm:mx-8  sm:w-3/5 lg:w-2/5"
                   />
@@ -94,24 +116,37 @@ const Stories = () => {
                 {/* item 3 */}
                 <div className="flex flex-col justify-center items-center sm:flex  sm:space-y-4 mt-10 lg:flex-row lg:mt-16">
                   <Image
-                    src={sampleImage2}
-                    width={500}
-                    height={500}
+                    src={"/images/stories/1.png"}
+                    width={1000}
+                    height={1000}
                     alt="Image 4"
                     className="w-4/5 border-2 rounded-2xl border-gray-300 shadow-lg  sm:mx-8  sm:w-3/5 lg:w-2/5"
                   />
                   <div className=" text-xl ">
                     <div className="md:justify-center md:items-center">
-                      <p className="text-base text-justify leading-relaxed text-black  p-4 sm:text-xl sm:ml-[9vh] sm:w-5/6 sm:text-justify   lg:text-xl">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                        Sed nec augue non libero varius sagittis. Nulla
-                        tincidunt vestibulum libero, in eleifend risus laoreet
-                        ut. Nulla facilisi. Donec ac nisl ut arcu dignissim
-                        dictum. Praesent lacinia diam ac augue fringilla, vel
-                        mattis quam vehicula. In non est tellus. Integer sit
-                        amet bibendum sem. Nulla euismod justo a ligula
-                        hendrerit, in interdum justo laoreet.
+                      <p className="text-base text-justify leading-relaxed text-black  p-4 sm:text-lg sm:ml-[9vh] sm:w-5/6 sm:text-justify   lg:text-xl">
+                        "Choosing to specialize in Computer Systems and Network
+                        Engineering (CSNE) wasn't just a career path for me; it
+                        was a calling. Despite the challenges and complexities
+                        of the field, I embraced every opportunity to learn and
+                        excel. Throughout my academic journey at SLIIT
+                        University, I actively participated in research
+                        projects, internships, and extracurricular activities,
+                        honing my skills and expanding my knowledge. Upon
+                        graduation with flying colors, I secured a coveted
+                        position at a leading tech firm, where I immediately
+                        made an impact with my expertise and innovative
+                        solutions. My journey from student to professional is a
+                        testament to the power of dedication, resilience, and
+                        the unwavering pursuit of excellence. It's not just
+                        about achieving success; it's about embracing the
+                        challenges, seizing opportunities, and continuously
+                        pushing the boundaries of what's possible in the dynamic
+                        world of Computer Systems and Network Engineering."
                       </p>
+                      <div className="text-right text-black mx-20 italic">
+                        - Thiwanka Cholitha -
+                      </div>
                     </div>
                   </div>
                 </div>
