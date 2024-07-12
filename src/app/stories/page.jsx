cbd61b4dac74483e0ae134ecea9a6ff839556ad9
Sahan Perera <sahanvimukthi00@gmail.com>
2024-07-13 02:58:46 +0530

    fix: added style changes

--- src/app/stories/page.jsx
+++ src/app/stories/page.jsx
@@ -21,7 +21,7 @@ const Stories = () => {
 
   // Helper function to get preview text
   const getPreviewText = (text) => {
-    const previewLength = 148; // Number of characters to show in the preview
+    const previewLength = 600; // Number of characters to show in the preview
     if (text.length <= previewLength) return text;
     return text.slice(0, previewLength) + "..";
   };
@@ -33,15 +33,15 @@ const Stories = () => {
           <div className="flex w-full flex-col items-center md:w-full lg:w-full xl:w-full 2xl:w-full lg:mb-10">
             {/* First section */}
             <div className="w-full flex items-center justify-center sm:w-full xl:w-full 2xl:w-full bg-white">
-              <div className="h-[70vh] flex flex-col-reverse items-center justify-center w-full sm:flex-col-reverse sm:justify-center sm:w-full sm:items-center sm:mb-12 sm:mt-10 lg:flex-row lg:justify-around lg:w-[140vh] lg:mt-10 lg:mb-0">
+              <div className="lg:h-[60vh] flex flex-col-reverse items-center justify-center w-full sm:flex-col-reverse sm:justify-center sm:w-full sm:items-center sm:mb-12 sm:mt-10 lg:flex-row lg:justify-around lg:w-[140vh] lg:mt-10 lg:mb-0">
                 <div className="w-full sm:w-full lg:w-full p-4 sm:text-center sm:flex lg:flex xl:w-3/4 lg:mb-[5vh] xl:mb-[5vh]">
                   <div className="text-wrap sm:flex sm:items-center sm:justify-center lg:flex">
                     {/* paragraph section div */}
                     <div className="lg:w-3/4">
-                      <h1 className="text-4xl m-4 sm:m-8 lg:text-6xl text-black font-bold text-center sm:mr-4">
+                      <h1 className="text-4xl m-4 sm:m-8 lg:text-5xl text-black font-bold text-center sm:mr-4">
                         Our Way of Success
                       </h1>
-                      <p className="text-base pt-8 sm:text-xl text-black leading-relaxed text-justify sm:ml-4 md:text-center lg:text-center lg:text-lg xl:5xl 2xl:mb-[12vh]">
+                      <p className="pt-8 sm:text-xl px-5 text-black leading-relaxed text-justify sm:ml-4 md:text-center lg:text-justify lg:text-lg xl:5xl 2xl:mb-[12vh]">
                         Explore the extraordinary achievements of CSNE Club
                         members and faculty. From alumni success to innovation
                         and community impact, our Success Stories page showcases
@@ -64,7 +64,7 @@ const Stories = () => {
             </div>
 
             {/* Second section */}
-            <div className="flex flex-col md:w-full lg:w-full xl:w-full 2xl:w-full justify-center items-center">
+            <div className="flex flex-col md:w-full lg:w-full xl:w-full 2xl:w-full justify-center items-center mt-10">
               {StoryData?.length > 0 &&
                 StoryData.map((story) => {
                   const isExpanded = expandedStories.includes(story.id);
@@ -79,10 +79,10 @@ const Stories = () => {
                           width={500}
                           height={300}
                           alt="Story image"
-                          className="w-4/5 border-2 rounded-2xl border-gray-300 shadow-lg sm:mx-8 sm:w-3/5 lg:w-2/5"
+                          className="w-4/5 border-2 rounded-2xl border-gray-300 shadow-lg sm:mx-8 sm:w-3/5 lg:w-2/6"
                         />
-                        <div className="text-lg flex flex-col px-10 py-5">
-                          <p className="text-lg text-justify leading-relaxed text-black sm:w-5/6">
+                        <div className="flex flex-col px-10 py-5">
+                          <p className="lg:text-lg text-justify leading-relaxed text-black sm:w-5/6">
                             {isExpanded
                               ? story.description
                               : getPreviewText(story.description)}
@@ -107,13 +107,13 @@ const Stories = () => {
                                 Show Less
                               </button>
                               <br />
-                              <p className="font-semibold text-black">
+                              <p className="font-semibold text-black text-center lg:text-left">
                                 {story.name}
                               </p>
-                              <p className="font-semibold text-black">
+                              <p className="font-semibold text-black text-center lg:text-left">
                                 {story.designation}
                               </p>
-                              <p className="font-semibold text-black">
+                              <p className="font-semibold text-black text-center lg:text-left">
                                 {story.workplace}
                               </p>
                             </>
